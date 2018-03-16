7324fcbfd3348f2b9a05ab3e0829db8ccc8bbe2f
Ben <benny.boy1695@gmail.com>
2018-03-16 12:46:03 +0000

    Update Telescopefix.java
    Removed un-needed bits of default code.

--- src/main/java/io/github/bennyboy1695/telescopefix/Telescopefix.java
+++ src/main/java/io/github/bennyboy1695/telescopefix/Telescopefix.java
@@ -39,23 +39,6 @@ public class Telescopefix {
     @Mod.Instance(MOD_ID)
     public static Telescopefix INSTANCE;
 
-    /**
-     * This is the first initialization event. Register tile entities here.
-     * The registry events below will have fired prior to entry to this method.
-     */
-    @Mod.EventHandler
-    public void preinit(FMLPreInitializationEvent event) {
-
-    }
-
-    /**
-     * This is the second initialization event. Register custom recipes
-     */
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-
-    }
-
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void rightClickTelescope(PlayerInteractEvent.RightClickBlock event) {
         if (doesExist("hellfirepvp.astralsorcery.AstralSorcery")) {
@@ -65,7 +48,7 @@ public class Telescopefix {
                 TileEntity tile = event.getWorld().getTileEntity(event.getPos());
                 if (tile instanceof TileTelescope) {
                     TileTelescope telescope = (TileTelescope) event.getWorld().getTileEntity(event.getPos());
-                    //player.openGui(INSTANCE, 1, event.getWorld(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ());
+                  
                     try {
                         new GuiTelescope(player, telescope);
 
@@ -84,56 +67,6 @@ public class Telescopefix {
     @Mod.EventHandler
     public void postinit(FMLPostInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    /**
-     * Forge will automatically look up and bind blocks to the fields in this class
-     * based on their registry name.
-     */
-    @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Blocks {
-      /*
-          public static final MySpecialBlock mySpecialBlock = null; // placeholder for special block below
-      */
-    }
-
-    /**
-     * Forge will automatically look up and bind items to the fields in this class
-     * based on their registry name.
-     */
-    @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Items {
-      /*
-          public static final ItemBlock mySpecialBlock = null; // itemblock for the block above
-          public static final MySpecialItem mySpecialItem = null; // placeholder for special item below
-      */
-    }
-
-    /**
-     * This is a special class that listens to registry events, to allow creation of mod blocks and items at the proper time.
-     */
-    @Mod.EventBusSubscriber
-    public static class ObjectRegistryHandler {
-        /**
-         * Listen for the register event for creating custom items
-         */
-        @SubscribeEvent
-        public static void addItems(RegistryEvent.Register<Item> event) {
-           /*
-             event.getRegistry().register(new ItemBlock(Blocks.myBlock).setRegistryName(MOD_ID, "myBlock"));
-             event.getRegistry().register(new MySpecialItem().setRegistryName(MOD_ID, "mySpecialItem"));
-            */
-        }
-
-        /**
-         * Listen for the register event for creating custom blocks
-         */
-        @SubscribeEvent
-        public static void addBlocks(RegistryEvent.Register<Block> event) {
-           /*
-             event.getRegistry().register(new MySpecialBlock().setRegistryName(MOD_ID, "mySpecialBlock"));
-            */
-        }
     }
 
     public boolean doesExist(String className) {
